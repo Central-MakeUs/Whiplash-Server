7cf805bde12e250ac66977d58bfeef50e5382071
strongmhk <kim6562166086@gmail.com>
2025-07-01 16:07:47 +0900

    프로젝트 초기 세팅

---
+++ src/test/java/akuma/whiplash/WhiplashApplicationTests.java
@@ -0,0 +1,13 @@
+package akuma.whiplash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiplashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
