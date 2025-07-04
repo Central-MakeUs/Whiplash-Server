6adb72f9626bd322826d9dfd83444da6c21f0fc5
strongmhk <kim6562166086@gmail.com>
2025-07-04 17:51:47 +0900

    [#4] ✨ feature: MemberEntity 정의
    - Role: 권한
    - SocialType: 소셜 로그인 서드파티 종류
    - TokenType: 토큰 종류

---
+++ src/main/java/akuma/whiplash/domains/member/domain/contants/SocialType.java
@@ -0,0 +1,6 @@
+package akuma.whiplash.domains.member.domain.contants;
+
+public enum SocialType {
+
+    GOOGLE, APPLE, KAKAO;
+}
---
+++ src/main/java/akuma/whiplash/domains/member/persistence/entity/MemberEntity.java
@@ -0,0 +1,51 @@
+package akuma.whiplash.domains.member.persistence.entity;
+
+import akuma.whiplash.domains.member.domain.contants.Role;
+import akuma.whiplash.domains.member.domain.contants.SocialType;
+import akuma.whiplash.global.entity.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.DynamicInsert;
+
+@Entity
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DynamicInsert
+@Table(name = "member", uniqueConstraints = {
+    @UniqueConstraint(columnNames = {"social_id", "social_type"})
+})
+public class MemberEntity extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "social_id", nullable = false)
+    private String socialId;
+
+    @Column(name = "social_type")
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
+
+    @Column(length = 50, nullable = false)
+    private String email;
+
+    @Column(length = 50, nullable = false)
+    private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+}
