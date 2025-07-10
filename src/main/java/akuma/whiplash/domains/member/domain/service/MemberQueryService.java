17869f4bb1f555f5c30f305cc1d6d1dcd3e0d026
strongmhk <kim6562166086@gmail.com>
2025-07-11 01:15:19 +0900

    [#7] :recycle: Refactor: 인터페이스로 변경

--- src/main/java/akuma/whiplash/domains/member/domain/service/MemberQueryService.java
+++ src/main/java/akuma/whiplash/domains/member/domain/service/MemberQueryService.java
@@ -1,22 +1,8 @@
 package akuma.whiplash.domains.member.domain.service;
 
 import akuma.whiplash.domains.member.persistence.entity.MemberEntity;
-import akuma.whiplash.domains.member.persistence.repository.MemberRepository;
-import akuma.whiplash.global.exception.ApplicationException;
-import akuma.whiplash.global.response.code.CommonErrorCode;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-@Service
-@RequiredArgsConstructor
-@Transactional(readOnly = true)
-public class MemberQueryService {
+public interface MemberQueryService {
 
-    private final MemberRepository memberRepository;
-
-    public MemberEntity findById(Long memberId) {
-        return memberRepository.findById(memberId)
-            .orElseThrow(() -> ApplicationException.from(CommonErrorCode.BAD_REQUEST));
-    }
+    MemberEntity findById(Long memberId);
 }
