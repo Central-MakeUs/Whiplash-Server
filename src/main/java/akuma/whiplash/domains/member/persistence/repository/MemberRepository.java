741783560740e1ee01554eb47048b8bb57d0fd25
strongmhk <kim6562166086@gmail.com>
2025-07-13 00:09:27 +0900

    [#9] 🔨 Fix: social_type 컬럼 삭제
    - social_type 정보 social_id 컬럼에 통합

--- src/main/java/akuma/whiplash/domains/member/persistence/repository/MemberRepository.java
+++ src/main/java/akuma/whiplash/domains/member/persistence/repository/MemberRepository.java
@@ -1,11 +1,10 @@
 package akuma.whiplash.domains.member.persistence.repository;
 
-import akuma.whiplash.domains.member.domain.contants.SocialType;
 import akuma.whiplash.domains.member.persistence.entity.MemberEntity;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
 
-    Optional<MemberEntity> findBySocialIdAndSocialType(String socialId, SocialType socialType);
+    Optional<MemberEntity> findBySocialId(String socialId);
 }
