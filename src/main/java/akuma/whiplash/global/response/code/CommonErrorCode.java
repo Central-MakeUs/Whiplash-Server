a500e8992c1b41c514d5b250ecd4e7f0fa2fabc3
strongmhk <kim6562166086@gmail.com>
2025-07-14 00:31:22 +0900

    [#13] 🔨 Fix: 404 코드 추가

--- src/main/java/akuma/whiplash/global/response/code/CommonErrorCode.java
+++ src/main/java/akuma/whiplash/global/response/code/CommonErrorCode.java
@@ -12,8 +12,9 @@ public enum CommonErrorCode implements BaseErrorCode {
     METHOD_ARGUMENT_NOT_VALID(HttpStatus.BAD_REQUEST, "COMMON_002", "올바르지 않은 요청 형식입니다."),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON_003", "인증 과정에서 오류가 발생했습니다."),
     FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON_004", "금지된 요청입니다."),
-    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "COMMON_005", "지원하지 않는 HTTP Method 입니다."),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_006", "서버 에러가 발생했습니다.");
+    NOT_FOUND(HttpStatus.NOT_FOUND, "COMMON_005", "찾을 수 없습니다."),
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "COMMON_006", "지원하지 않는 HTTP Method 입니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_007", "서버 에러가 발생했습니다.");
 
     private final HttpStatus httpStatus;
     private final String customCode;
