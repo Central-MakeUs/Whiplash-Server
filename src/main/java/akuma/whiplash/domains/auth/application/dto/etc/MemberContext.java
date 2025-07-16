f3e2d9f619804aa7344051f57604c4c6836f0883
strongmhk <kim6562166086@gmail.com>
2025-07-16 23:38:51 +0900

    [#15] 🔨 Fix: MemberContext 데이터 추가
    Member 권한 데이터 추가

--- src/main/java/akuma/whiplash/domains/auth/application/dto/etc/MemberContext.java
+++ src/main/java/akuma/whiplash/domains/auth/application/dto/etc/MemberContext.java
@@ -1,9 +1,11 @@
 package akuma.whiplash.domains.auth.application.dto.etc;
 
+import akuma.whiplash.domains.member.domain.contants.Role;
 import lombok.Builder;
 
 @Builder
 public record MemberContext(
+    Role role,
     Long memberId,
     String socialId,
     String email,
