1f2f36ef49784224d28e50cc86f028ff98c3a39b
strongmhk <kim6562166086@gmail.com>
2025-07-30 23:29:19 +0900

    [#30] 🔧 Chore: 통합테스트용 애노테이션 설정

---
+++ src/test/java/akuma/whiplash/common/config/IntegrationTest.java
@@ -0,0 +1,17 @@
+package akuma.whiplash.common.config;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest
+@ActiveProfiles("test")
+@Import({TestDatabaseConfig.class, TestRedisConfig.class})
+public @interface IntegrationTest {
+}
